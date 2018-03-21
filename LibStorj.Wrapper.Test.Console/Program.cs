28339158d90c3cafdc9a24a8ada43bef75cb492e
TopperDEL <github_com@tparth.de>
2018-03-21 13:48:31 +0100

    Update Program.cs

--- LibStorj.Wrapper.Test.Console/Program.cs
+++ LibStorj.Wrapper.Test.Console/Program.cs
@@ -1,4 +1,4 @@
-﻿using LibStorj.Wrapper.Contracts.Interfaces;
+0﻿using LibStorj.Wrapper.Contracts.Interfaces;
 using LibStorj.Wrapper.Contracts.Models;
 using LibStorj.Wrapper.x64;
 using Nito.AsyncEx;
@@ -30,7 +30,7 @@ namespace LibStorj.Wrapper.Test.Console.x64
             var keyExist = storj.KeysExist; //This is not really working - it always returns "true"
             
             var buckets = await storj.GetBucketsAsync(); //Load all buckets - if that works your credentials did work
-            var files = await storj.ListFilesAsync(buckets[4]); //Load the files of the first bucket
+            var files = await storj.ListFilesAsync(buckets[0]); //Load the files of the first bucket
 
             //Get the versions of the dependencies to see if they work
             var v1 = (new VersionInfo()).GetCurlVersion();
@@ -38,19 +38,19 @@ namespace LibStorj.Wrapper.Test.Console.x64
             var v3 = (new VersionInfo()).GetJsonCVersion();
             var v4 = (new VersionInfo()).GetNettleVersion();
 
-            storj.UploadFile(buckets[4].Id, "Uploadfile.txt", @"YOURPATH\YOURFILE.txt"); //Test-upload a file - provide a path to a small file here
+            storj.UploadFile(buckets[0].Id, "Uploadfile.txt", @"YOURPATH\YOURFILE.txt"); //Test-upload a file - provide a path to a small file here
 
             //At this point, the upload does not work.
-            //After the next line the upload works but blocks GetInfoAsync. If the file is uploaded, the GetInfo is executed and returned
+            //After the next line the upload works but blocks GetInfoAsync. If the file is uploaded, the GetInfo is executed and returns
             var info = await storj.GetInfoAsync();
 
-            //The same with the download
+            //The same with download
             var job = storj.DownloadFile(files.First(), @"YOURPATH\YOURFILE.txt");
             while(!job.IsFinished)
             {
                 System.Console.Clear();
                 System.Console.WriteLine("Status: " + job.CurrentProgress.DoneBytes + "/" + job.CurrentProgress.TotalBytes + " - " + job.CurrentProgress.Progress + "%");
-                //The same with download...
+                //The same like above
                 var infoDownload = await storj.GetInfoAsync();
             }
         }
